6aa52b5cd170c157e930a88a2b74a7df811f8a3f
Kevin Andrade <kevinandradecardenas@gmail.com>
2024-01-15 18:23:28 -0500

    inicializar proyecto

---
+++ src/main/java/com/uce/edu/Efinal1Pa2P5KaApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Efinal1Pa2P5KaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Efinal1Pa2P5KaApplication.class, args);
+	}
+
+}
